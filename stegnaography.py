06896eb5ebbb5ba11ec6d068c5fb14e2c0aa39e1
mojitog <mh.mojallal@gmail.com>
2025-10-15 04:23:51 +0200

    text to decimal func

--- stegnaography.py
+++ stegnaography.py
@@ -11,9 +11,13 @@ def get_inputs():
         input("Please insert the image address: ")
     pass
 
+
+def text_to_decimal(text):
+    return list(map(ord, text))
+
+
 key = 10
-text = "Moji is really trying to be cool!"
+text = "Moji is really trying to be cool here!"
 img = Image.open("Heath_Ledger_as_the_Joker.JPG.webp")
 
-
-
+code_list = text_to_decimal(text)
